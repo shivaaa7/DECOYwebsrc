95159958d784072eeb4debee1fb2e983c8c87bdf
Rohitkumar367 <rohitddroid@gmail.com>
2023-08-28 11:52:52 +0530

    Adden menu driven bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,12 @@
 		});
 
 })(jQuery);
+
+let header = document.querySelector('.header');
+document.querySelector('#menu-btn').onclick = () => {
+	header.classList.toggle('active');
+}
+
+window.onscroll = () => {
+	header.classList.remove('active');
+}
